e1a903992d6a2bd0b352689c7a14a7c0ba630e2a
blinkEgor <barminov44@gmail.com>
2023-05-20 12:05:43 +0300

    added-components-and-features

---
+++ hall/bar/menu-script.js
@@ -0,0 +1,83 @@
+// components...
+class Gin {
+    constructor(name, volume, unit, price, currency) {
+        name = "Gin";
+        volume = 500;
+        unit = "ml";
+        price = 11.60;
+        currency = "$";
+    }
+}
+class RichJuice {
+    constructor(name, volume, unit, price, currency) {
+        name = "Rich Juice";
+        volume = 1000;
+        unit = "ml";
+        price = 4.00;
+        currency = "$";
+    }
+}
+class LemonJuice {
+    constructor(name, volume, unit, price, currency) {
+        name = "Lemon Juice";
+        volume = 1000;
+        unit = "ml";
+        price = 10.00;
+        currency = "$";
+    }
+}
+class Tonic {
+    constructor(name, volume, unit, price, currency) {
+        name = "Tonic";
+        volume = 1000;
+        unit = "ml";
+        price = 3.40;
+        currency = "$";
+    }
+}
+class Soda {
+    constructor(name, volume, unit, price, currency) {
+        name = "Soda";
+        volume = 1000;
+        unit = "ml";
+        price = 1.90;
+        currency = "$";
+    }
+}
+class SugarSyrup {
+    constructor(name, volume, unit, price, currency) {
+        name = "Sugar Syrup";
+        volume = 1500;
+        unit = "ml";
+        price = 4.50;
+        currency = "$";
+    }
+}
+class Vodka {
+    constructor(name, volume, unit, price, currency) {
+        name = "Vodka";
+        volume = 500;
+        unit = "ml";
+        price = 9.80;
+        currency = "$";
+    }
+}
+class DryVermouth {
+    constructor(name, volume, unit, price, currency) {
+        name = "Dry Vermouth";
+        volume = 500;
+        unit = "ml";
+        price = 33.00;
+        currency = "$";
+    }
+}
+class CocaCola {
+    constructor(name, volume, unit, price, currency) {
+        name = "Coca Cola";
+        volume = 2000;
+        unit = "ml";
+        price = 4.70;
+        currency = "$";
+    }
+}
+
