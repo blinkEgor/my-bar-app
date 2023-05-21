54d23cd2f177388593b6d283691d9a9014424dd2
blinkEgor <barminov44@gmail.com>
2023-05-21 10:54:51 +0300

    made-the-template-more-compact

--- hall/bar/menu-script.js
+++ hall/bar/menu-script.js
@@ -1,83 +1,22 @@
 // components...
-class Gin {
-    constructor(name, volume, unit, price, currency) {
-        name = "Gin";
-        volume = 500;
-        unit = "ml";
-        price = 11.60;
-        currency = "$";
-    }
-}
-class RichJuice {
-    constructor(name, volume, unit, price, currency) {
-        name = "Rich Juice";
-        volume = 1000;
-        unit = "ml";
-        price = 4.00;
-        currency = "$";
-    }
-}
-class LemonJuice {
-    constructor(name, volume, unit, price, currency) {
-        name = "Lemon Juice";
-        volume = 1000;
-        unit = "ml";
-        price = 10.00;
-        currency = "$";
-    }
-}
-class Tonic {
-    constructor(name, volume, unit, price, currency) {
-        name = "Tonic";
-        volume = 1000;
-        unit = "ml";
-        price = 3.40;
-        currency = "$";
-    }
-}
-class Soda {
-    constructor(name, volume, unit, price, currency) {
-        name = "Soda";
-        volume = 1000;
-        unit = "ml";
-        price = 1.90;
-        currency = "$";
-    }
-}
-class SugarSyrup {
-    constructor(name, volume, unit, price, currency) {
-        name = "Sugar Syrup";
-        volume = 1500;
-        unit = "ml";
-        price = 4.50;
-        currency = "$";
-    }
-}
-class Vodka {
-    constructor(name, volume, unit, price, currency) {
-        name = "Vodka";
-        volume = 500;
-        unit = "ml";
-        price = 9.80;
-        currency = "$";
-    }
-}
-class DryVermouth {
-    constructor(name, volume, unit, price, currency) {
-        name = "Dry Vermouth";
-        volume = 500;
-        unit = "ml";
-        price = 33.00;
-        currency = "$";
-    }
-}
-class CocaCola {
-    constructor(name, volume, unit, price, currency) {
-        name = "Coca Cola";
-        volume = 2000;
-        unit = "ml";
-        price = 4.70;
-        currency = "$";
-    }
-}
 
+class Components {
+    constructor(name, volume, price) {
+        name = this.name;
+        volume = this.volume;
+        price = this.price;
+        unit = "ml";
+        currency = "$";
+    }
+}
+const gin = new Components("Gin", 500, 11.60);
+const richJuice = new Components("Rich Juice", 1000, 4.00);
+const lemonJuice = new Components("Lemon Juice", 1000, 10.00);
+const tonic = new Components("Tonic", 1000, 3.40);
+const soda = new Components("Soda", 1000, 1.90);
+const sugarSyrup = new Components("Sugar Syrup", 1500, 4.50);
+const vodka = new Components("Vodka", 500, 9.80);
+const dryVermouth = new Components("Dry Vermouth", 500, 33.00);
+const cocaCola = new Components("Coca Cola", 2000, 4.70);
+
+// glass of drink...
