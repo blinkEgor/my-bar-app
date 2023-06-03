291321cbfa65066a613d9a71a6127783f59079c1
blinkEgor <barminov44@gmail.com>
2023-06-03 14:57:05 +0300

    rewrote-from-classes-to-objects-and-optimized-the-code

--- hall/bar/menu-script.js
+++ hall/bar/menu-script.js
@@ -1,71 +1,48 @@
+// constants..
+
+const tax = 1.5;
+const drinkName = document.getElementById("drink-name");
+const drinkCompound = document.getElementById("drink-compound");
+const drinkPrice = document.getElementById("drink-price");
+const names = [];
+const compounds = [];
+const prices = [];
+
+// print...
+
+
+
 // components...
 
-class Components {
-    constructor(name, volume, price) {
-        this.name = name;
-        this.volume = volume;
-        this.price = price;
-    }
-}
-const gin = new Components("Gin", 500, 11.60);
-const richJuice = new Components("Rich Juice", 1000, 4.00);
-const lemonJuice = new Components("Lemon Juice", 1000, 10.00);
-const tonic = new Components("Tonic", 1000, 3.40);
-const soda = new Components("Soda", 1000, 1.90);
-const sugarSyrup = new Components("Sugar Syrup", 1500, 4.50);
-const vodka = new Components("Vodka", 500, 9.80);
-const dryVermouth = new Components("Dry Vermouth", 500, 33.00);
-const cocaCola = new Components("Coca Cola", 2000, 4.70);
+const components = {
+    gin: { name: "Gin", volume: 500, price: 11.60 },
+    richJuice: { name: "Rich Juice", volume: 1000, price: 4.00 },
+    lemonJuice: { name: "Lemon Juice", volume: 1000, price: 10.00 },
+    tonic: { name: "Tonic", volume: 1000, price: 3.40 },
+    soda: { name: "Soda", volume: 1000, price: 1.90 },
+    sugarSyrup: { name: "Sugar Syrup", volume: 1500, price: 4.50 },
+    vodka: { name: "Vodka", volume: 500, price: 9.80 },
+    dryVermouth: { name: "Dry Vermouth", volume: 500, price: 33.00 },
+    cocaCola: { name: "Coca Cola", volume: 2000, price: 4.70 },
+};
 
 // glass of drink...
 
-class GlassOf {
-    constructor(drinkName, drinkCompound, drinkPrice) {
-        this.drinkName = drinkName;
-        this.drinkCompound = drinkCompound;
-        this.drinkPrice = drinkPrice;
-        this.volume = 200;
-        this.tax = 1.5;
-    }
-    makeGlassOfName(comp) {
-        const el = document.createElement("p");
-        this.drinkName.append(el);
-        el.innerText = `A glass of ${comp.name}`;
-    }
-    makeGlassOfCompound(comp) {
-        const el = document.createElement("p");
-        this.drinkCompound.append(el);
-        el.innerText = `${comp.name} ${this.volume} ml`;
-    }
-    makeGlassOfPrice(comp) {
-        const el = document.createElement("p");
-        const price = (Math.round((((this.volume * comp.price) / comp.volume) * this.tax) * 10) / 10).toFixed(2);
-        this.drinkPrice.append(el);
-        el.innerText = `$${price}`;
+const glassOf = ({name, volume, price}) => {
+    const thisName = `Glass of ${name}`;
+    const thisCompound = `${name} 200 ml`;
+    const total = `$${ (Math.round((((200 * price) / volume) * tax) * 10) / 10).toFixed(2) }`;
+    names.push(thisName);
+    compounds.push(thisCompound);
+    prices.push(total);
+};
 
-        console.log(`$${price}`);
-    }
-    
-}
-const glassOf = new GlassOf(
-    document.getElementById("drink-name"),
-    document.getElementById("drink-compound"),
-    document.getElementById("drink-price")
-);
-glassOf.makeGlassOfName(richJuice);
-glassOf.makeGlassOfName(tonic);
-glassOf.makeGlassOfName(soda);
-glassOf.makeGlassOfName(cocaCola);
+glassOf(components.richJuice);
+glassOf(components.tonic);
+glassOf(components.soda);
+glassOf(components.cocaCola);
 
-glassOf.makeGlassOfCompound(richJuice);
-glassOf.makeGlassOfCompound(tonic);
-glassOf.makeGlassOfCompound(soda);
-glassOf.makeGlassOfCompound(cocaCola);
-
-glassOf.makeGlassOfPrice(richJuice);
-glassOf.makeGlassOfPrice(tonic);
-glassOf.makeGlassOfPrice(soda);
-glassOf.makeGlassOfPrice(cocaCola);
-
-// cocktails...
+console.log(names);
+console.log(compounds);
+console.log(prices);
 
