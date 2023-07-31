b1b34dce1989963c1d97c1a4396047ef391319d6
blinkEgor <barminov44@gmail.com>
2023-07-31 21:18:04 +0300

    fix

--- script/hall-script.js
+++ script/hall-script.js
@@ -1,4 +1,5 @@
 document.querySelector(".burger").addEventListener("click", () => {
     document.querySelector(".burger").classList.toggle("active");
+    document.querySelector("main").classList.toggle("main-none");
     document.querySelector("nav").classList.toggle("open");
 })
